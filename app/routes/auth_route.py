cc81246e11b595d8550afc1f8d78226bb766ea2a
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-07-02 21:16:38 -0500

    chore: remove __pycache__ folders from Git tracking

--- app/routes/auth_route.py
+++ app/routes/auth_route.py
@@ -13,7 +13,6 @@ async def signup(user:UserCreate):
     user_dict = user.model_dump()
     user_dict["password"] = hash_password(user_dict["password"])
     await db["users"].insert_one(user_dict)
-
     return {"message" : "User Created Successfully!"}
 
 
