f4945b680cf278ba7ac856137acbe5d36345ff3f
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-07-06 17:08:07 -0500

    get all products

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -7,6 +7,16 @@ router = APIRouter()
 @router.post("/Products")
 async def add_Product(product:Product):
     prodcut_dict = product.model_dump()
-    result = await db["Product"].insert_one(prodcut_dict)
+    result = await db["product"].insert_one(prodcut_dict)
     return {"message": "Product added", "id": str(result.inserted_id)}
+
+@router.get("/Products")
+async def get_Products(product: Product):
+    products = []
+    async  for product in db["product"].find_one():
+        product ["_id"] = str(product["id"])
+        products.append(product)
+
+    return products
+
 
