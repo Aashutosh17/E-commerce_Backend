a79acb011b281b7f0c4a6e32a3f602bdd5e401b3
Aashutosh Paneru <aashutoshpaneru01@gmail.com>
2025-07-06 17:14:41 -0500

    feat: added get all products and get by id APIs

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -1,3 +1,6 @@
+from http.client import HTTPException
+
+from bson import ObjectId
 from fastapi import APIRouter
 from app.models.product_model import Product
 from app.database.connection import db
@@ -18,5 +21,18 @@ async def get_Products(product: Product):
         products.append(product)
 
     return products
+
+@router.get("/Products/Product_id")
+async def get_product_by_id(product_id: str):
+    if not ObjectId.is_valid(product_id):
+        raise HTTPException(status_code = 400, detail = "Invalid ID Format!")
+
+    product = await db["prodcuts"].find_one({"_id": ObjectId(product_id)})
+
+    if not product:
+        raise HTTPException(status_code = 404, detail= "Product not found!")
+
+    product ['_id'] = str(product['_id'])
+    return product
 
 
